b15e928165c3c5b9ebe29cb1f6f67da8cddb9d73
Alex Dodge <alex.dodge@redspace.com>
2018-04-27 09:00:41 -0300

    task-18: path module tests

--- src/modules/path.spec.js
+++ src/modules/path.spec.js
@@ -1,18 +1,47 @@
 import {
-  getReversedPathFragments
+  getReversedPathFragments,
+  matchFinalPathFragmentWithPattern,
+  isUnifiedSplitSwitchPath,
+  isValidHrefPath
 } from './paths.js';
 
-const testPath = {
-  empty: '',
-  regular: 'foo/bar/baz',
-}
 
-const expectedTestPathResult = {
-  empty: '',
-  regular: ['baz', 'bar', 'foo'],
-}
+describe('Test - getReversedPathFragments', () => {
+  const testPath = {
+    empty: '',
+    regular: 'foo/bar/baz',
+  }
+  
+  const expectedTestPathResult = {
+    empty: [''],
+    regular: ['baz', 'bar', 'foo'],
+  }
 
-test('Path is returned as reversed fragments', () => {
-  var reversedFragments = getReversedPathFragments(testPath.regular);
-  expect(reversedFragments).toEqual(expectedTestPathResult.regular);
+  test('Path is returned as reversed fragments', () => {
+    var reversedFragments = getReversedPathFragments(testPath.regular);
+    expect(reversedFragments).toEqual(expectedTestPathResult.regular);
+  });
+  
+  test('Empty path is returned as single empty array', () => {
+    var reversedFragments = getReversedPathFragments(testPath.empty);
+    expect(reversedFragments).toEqual(expectedTestPathResult.empty);
+  });
 });
+
+describe('Test - matchFinalPathFragmentWithPattern', () => {
+  const testPath = 'foo/bar/my?baz&should&contain';
+  const testMatch = 'baz';
+  const testNoMatch = 'foo';
+
+  test('Final fragment should match valid pattern', () => {
+    var isMatched = matchFinalPathFragmentWithPattern(testPath, testMatch);
+    expect(isMatched).toBeTruthy();
+  });
+
+  test('Final fragment should not match invalid pattern', () => {
+    var isMatched = matchFinalPathFragmentWithPattern(testPath, testNoMatch);
+    expect(isMatched).toBeFalsy();
+  })
+});
+
+describe('Test - ')
